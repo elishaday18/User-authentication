05e2f7770c0f770bce6bbf36fd56359e54e377d5
elishaday18 <elishamday18@gmail.com>
2018-11-05 19:53:03 -0500

    fixed error in user.js and added to nav bar
    added to navbar too only show signup when not logged in

--- models/user.js
+++ models/user.js
@@ -1,27 +1,27 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 var UserSchema = new mongoose.Schema({
+    favoriteBook: {
+        type: String,
+        required: true,
+        trim: true
+    },
     email: {
         type: String,
         unique: true,
         required: true,
         trim: true
-    }
-   /* name: {
+    },
+    name: {
         type: String,
         required: true,
         trim: true
-    }; */
-   /* favoriteBook: {
-        type: String,
-        required: true,
-        trim: true
-    }; 
+    },
      password: {
         type: String,
         required: true
-    }; */
-});
+     }
+}); 
 
 //authenticate input against database documents
 UserSchema.statics.authenticate = function (email, password, callback) {
